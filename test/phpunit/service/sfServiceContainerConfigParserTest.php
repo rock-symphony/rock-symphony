408afbe3231df6382149112f43d6a9a112beb5e3
e1himself <ivan.voskoboinyk@gmail.com>
2017-03-26 21:48:17 +0300

    Fix test assertions actual <-> expected order

--- test/phpunit/service/sfServiceContainerConfigParserTest.php
+++ test/phpunit/service/sfServiceContainerConfigParserTest.php
@@ -54,7 +54,7 @@ class sfServiceContainerConfigParserTest extends \PHPUnit\Framework\TestCase
   public function it_should_define_empty_parameters_array_for_an_empty_array_definition()
   {
     $builder = $this->parser->parse(array());
-    $this->assertEquals($builder->getParameters(), array());
+    $this->assertEquals(array(), $builder->getParameters());
   }
 
   /**
@@ -63,12 +63,15 @@ class sfServiceContainerConfigParserTest extends \PHPUnit\Framework\TestCase
   public function it_should_parse_parameters_and_convert_keys_to_lowercase()
   {
     $builder = $this->parser->parse(self::load('fixtures/yaml/services2.yml'));
-    $this->assertEquals($builder->getParameters(), array(
-      'foo' => 'bar',
-      'values' => array(true, false, 0, 1000.3),
-      'bar' => 'foo',
-      'foo_bar' => new sfServiceReference('foo_bar')
-    ));
+    $this->assertEquals(
+      array(
+        'foo' => 'bar',
+        'values' => array(true, false, 0, 1000.3),
+        'bar' => 'foo',
+        'foo_bar' => new sfServiceReference('foo_bar')
+      ),
+      $builder->getParameters()
+    );
   }
 
   // ->parse # services
@@ -79,7 +82,7 @@ class sfServiceContainerConfigParserTest extends \PHPUnit\Framework\TestCase
   public function it_should_return_empty_services_array_for_an_empty_array_definition()
   {
     $builder = $this->parser->parse(self::load('fixtures/yaml/services2.yml'));
-    $this->assertEquals($builder->getServiceDefinitions(), array());
+    $this->assertEquals(array(), $builder->getServiceDefinitions());
   }
 
   /**
@@ -90,18 +93,18 @@ class sfServiceContainerConfigParserTest extends \PHPUnit\Framework\TestCase
     $builder = $this->parser->parse(self::load('fixtures/yaml/services3.yml'));
 
     $this->assertTrue($builder->hasServiceDefinition('foo'), '->parse parses service elements');
-    $this->assertEquals(get_class($builder->getServiceDefinition('foo')), 'sfServiceDefinition', '->parse converts service element to sfServiceDefinition instances');
-    $this->assertEquals($builder->getServiceDefinition('foo')->getClass(), 'FooClass', '->parse parses the class attribute');
+    $this->assertInstanceOf('sfServiceDefinition', $builder->getServiceDefinition('foo'), '->parse converts service element to sfServiceDefinition instances');
+    $this->assertEquals('FooClass', $builder->getServiceDefinition('foo')->getClass(), '->parse parses the class attribute');
     $this->assertTrue($builder->getServiceDefinition('shared')->isShared(), '->parse parses the shared attribute');
     $this->assertFalse($builder->getServiceDefinition('non_shared')->isShared(), '->parse parses the shared attribute');
-    $this->assertEquals($builder->getServiceDefinition('constructor')->getConstructor(), 'getInstance', '->parse parses the constructor attribute');
-    $this->assertEquals($builder->getServiceDefinition('file')->getFile(), '%path%/foo.php', '->parse parses the file tag');
-    $this->assertEquals($builder->getServiceDefinition('arguments')->getArguments(), array('foo', new sfServiceReference('foo'), array(true, false)), '->parse parses the argument tags');
-    $this->assertEquals($builder->getServiceDefinition('configurator1')->getConfigurator(), 'sc_configure', '->parse parses the configurator tag');
-    $this->assertEquals($builder->getServiceDefinition('configurator2')->getConfigurator(), array(new sfServiceReference('baz'), 'configure'), '->parse parses the configurator tag');
-    $this->assertEquals($builder->getServiceDefinition('configurator3')->getConfigurator(), array('BazClass', 'configureStatic'), '->parse parses the configurator tag');
-    $this->assertEquals($builder->getServiceDefinition('method_call1')->getMethodCalls(), array(array('setBar', array())), '->parse parses the method_call tag');
-    $this->assertEquals($builder->getServiceDefinition('method_call2')->getMethodCalls(), array(array('setBar', array('foo', new sfServiceReference('foo'), array(true, false)))), '->parse parses the method_call tag');
+    $this->assertEquals('getInstance', $builder->getServiceDefinition('constructor')->getConstructor(), '->parse parses the constructor attribute');
+    $this->assertEquals('%path%/foo.php', $builder->getServiceDefinition('file')->getFile(), '->parse parses the file tag');
+    $this->assertEquals(array('foo', new sfServiceReference('foo'), array(true, false)), $builder->getServiceDefinition('arguments')->getArguments(), '->parse parses the argument tags');
+    $this->assertEquals('sc_configure', $builder->getServiceDefinition('configurator1')->getConfigurator(), '->parse parses the configurator tag');
+    $this->assertEquals(array(new sfServiceReference('baz'), 'configure'), $builder->getServiceDefinition('configurator2')->getConfigurator(), '->parse parses the configurator tag');
+    $this->assertEquals(array('BazClass', 'configureStatic'), $builder->getServiceDefinition('configurator3')->getConfigurator(), '->parse parses the configurator tag');
+    $this->assertEquals(array(array('setBar', array())), $builder->getServiceDefinition('method_call1')->getMethodCalls(), '->parse parses the method_call tag');
+    $this->assertEquals(array(array('setBar', array('foo', new sfServiceReference('foo'), array(true, false)))), $builder->getServiceDefinition('method_call2')->getMethodCalls(), '->parse parses the method_call tag');
     $this->assertTrue($builder->hasAlias('alias_for_foo'), '->parse parses aliases');
     $this->assertEquals($builder->getAlias('alias_for_foo'), 'foo', '->parse parses aliases');
   }
@@ -113,31 +116,31 @@ class sfServiceContainerConfigParserTest extends \PHPUnit\Framework\TestCase
   {
     $parser = new sfServiceContainerConfigParser($builder = new sfServiceContainerBuilder(array('bar' => 'foo')));
     $parser->parse(array('parameters' => array('foo' => 'bar')));
-    $this->assertEquals($builder->getParameters(), array('bar' => 'foo', 'foo' => 'bar'), '->parse() merges current parameters with the loaded ones');
+    $this->assertEquals(array('bar' => 'foo', 'foo' => 'bar'), $builder->getParameters(), '->parse() merges current parameters with the loaded ones');
 
     $parser = new sfServiceContainerConfigParser($builder = new sfServiceContainerBuilder(array('bar' => 'foo', 'foo' => 'baz')));
     $parser->parse(array('parameters' => array('foo' => 'bar')));
-    $this->assertEquals($builder->getParameters(), array('bar' => 'foo', 'foo' => 'baz'), '->parse() does not change the already defined parameters');
+    $this->assertEquals(array('bar' => 'foo', 'foo' => 'baz'), $builder->getParameters(), '->parse() does not change the already defined parameters');
 
     $parser = new sfServiceContainerConfigParser($builder = new sfServiceContainerBuilder(array('bar' => 'foo')));
     $parser->parse(array('parameters' => array('foo' => '%bar%')));
-    $this->assertEquals($builder->getParameters(), array('bar' => 'foo', 'foo' => 'foo'), '->parse() evaluates the values of the parameters towards already defined ones');
+    $this->assertEquals(array('bar' => 'foo', 'foo' => 'bar'), $builder->getParameters(), '->parse() evaluates the values of the parameters towards already defined ones');
 
     $parser = new sfServiceContainerConfigParser($builder = new sfServiceContainerBuilder(array('bar' => 'foo')));
     $parser->parse(array('parameters' => array('foo' => '%bar%', 'baz' => '%foo%')));
-    $this->assertEquals($builder->getParameters(), array('bar' => 'foo', 'foo' => 'foo', 'baz' => 'foo'), '->parse() evaluates the values of the parameters towards already defined ones');
+    $this->assertEquals(array('bar' => 'foo', 'foo' => 'foo', 'baz' => 'foo'), $builder->getParameters(), '->parse() evaluates the values of the parameters towards already defined ones');
 
     $parser = new sfServiceContainerConfigParser($builder = new sfServiceContainerBuilder());
     $builder->setAlias('foo', 'FooClass');
     $builder->setAlias('bar', 'BarClass');
     $parser->parse(array('services' => array('baz' => new sfServiceDefinition('BazClass'), 'alias_for_foo' => 'foo')));
-    $this->assertEquals(array_keys($builder->getServiceDefinitions()), array('foo', 'bar', 'baz'), '->parse() merges definitions already defined ones');
-    $this->assertEquals($builder->getAliases(), array('alias_for_foo' => 'foo'), '->parse() registers defined aliases');
+    $this->assertEquals(array('foo', 'bar', 'baz'), array_keys($builder->getServiceDefinitions()), '->parse() merges definitions already defined ones');
+    $this->assertEquals(array('alias_for_foo' => 'foo'), $builder->getAliases(), '->parse() registers defined aliases');
 
     $parser = new sfServiceContainerConfigParser($builder = new sfServiceContainerBuilder());
     $builder->setAlias('foo', 'FooClass');
     $parser->parse(array('services' => array('foo' => new sfServiceDefinition('BazClass'))));
-    $this->assertEquals($builder->getServiceDefinition('foo')->getClass(), 'BazClass', '->parse() overrides already defined services');
+    $this->assertEquals('BazClass', $builder->getServiceDefinition('foo')->getClass(), '->parse() overrides already defined services');
   }
 
   private static function load($path)
