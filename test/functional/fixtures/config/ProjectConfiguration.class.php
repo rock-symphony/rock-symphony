b61873db83dbf0603b6de9a51e2a471d6e25e812
e1himself <ivan.voskoboinyk@gmail.com>
2017-07-10 17:18:21 +0300

    Remove sfDoctrinePlugin from repository
    Now it's an optional composer dependency.

--- test/functional/fixtures/config/ProjectConfiguration.class.php
+++ test/functional/fixtures/config/ProjectConfiguration.class.php
@@ -7,7 +7,6 @@ class ProjectConfiguration extends sfProjectConfiguration
 {
   public function setup()
   {
-    $this->enableAllPluginsExcept(array('sfDoctrinePlugin'));
     $this->enablePlugins('sfAutoloadPlugin');
   }
 }
