e2c5a99f5d1dbd6272aec2399305e924f9319218
e1himself <ivan.voskoboinyk@gmail.com>
2017-07-11 23:38:30 +0300

    Fix sfServiceReferenceTest

--- test/unit/service/sfServiceReferenceTest.php
+++ test/unit/service/sfServiceReferenceTest.php
@@ -16,4 +16,4 @@ $t = new lime_test(1);
 $t->diag('__construct() ->__toString()');
 
 $ref = new sfServiceReference('foo');
-$t->is((string) $ref, 'foo', '__construct() sets the id of the reference, which is used for the __toString() method');
+$t->is((string) $ref, '@foo', '__construct() sets the id of the reference, which is used for the __toString() method');
