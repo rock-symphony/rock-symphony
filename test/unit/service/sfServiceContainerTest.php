bb6076d0f96bb3d4ff0248464738f89981b0fb3c
e1himself <ivan.voskoboinyk@gmail.com>
2017-11-09 01:39:54 +0200

    Fix irrelevant tests

--- test/unit/service/sfServiceContainerTest.php
+++ test/unit/service/sfServiceContainerTest.php
@@ -29,30 +29,16 @@ $sc->foo1 = $obj1 = new stdClass();
 $t->ok($sc->has('foo'), '->has() returns true if the service is defined');
 $t->ok(!$sc->has('bar'), '->has() returns false if the service is not defined');
 
-class ProjectServiceContainer extends sfServiceContainer
-{
-  public $__bar, $__foo_bar, $__foo_baz;
+$bar = new stdClass();
 
-  public function __construct()
-  {
-    parent::__construct();
+$sc = new sfServiceContainer();
+$sc->set('bar', $bar);
 
-    $this->__bar = new stdClass();
-    $this->__foo_bar = new stdClass();
-    $this->__foo_baz = new stdClass();
-
-    $this->set('bar', $this->__bar);
-    $this->set('foo_bar', $this->__foo_bar);
-    $this->set('foo_baz', $this->__foo_baz);
-  }
-}
-
-$sc = new ProjectServiceContainer();
-$t->is(spl_object_hash($sc->get('bar')), spl_object_hash($sc->__bar), '->get() returns the same service object');
+$t->is(spl_object_hash($sc->get('bar')), spl_object_hash($bar ), '->get() returns the same service object');
 $t->ok($sc->has('bar'), '->has() returns true if the service has been defined');
 
-$sc->set('bar', $bar = new stdClass());
-$t->is(spl_object_hash($sc->get('bar')), spl_object_hash($bar), '->get() prefers to return a new service object if overrided');
+$sc->set('bar', $another_bar = new stdClass());
+$t->is(spl_object_hash($sc->get('bar')), spl_object_hash($another_bar), '->get() prefers to return a new service object if overrided');
 
 try
 {
