149b58ccef586b3829488b5121c8531c830e65a1
Ivan Voskoboinyk <ivan.voskoboinyk@gmail.com>
2019-11-08 21:42:12 +0200

    Add types to sfPager class

--- test/unit/addon/sfPagerTest.php
+++ test/unit/addon/sfPagerTest.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the symfony package.
  * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -13,12 +13,12 @@ require_once(__DIR__.'/../../bootstrap/unit.php');
 $t = new lime_test(2);
 class myPager extends sfPager
 {
-  function init() {}
-  function retrieveObject($offset) {}
-  function getResults() 
+  function init(): void {}
+  function retrieveObject(int $offset) {}
+  function getResults(): array
   {
     $this->setNbResults(2);
-    return array("foo", "bar");
+    return ["foo", "bar"];
   }
 }
 
