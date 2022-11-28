293f7d95a2ba0cb2ab495e73c54f71ab47ed005d
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-11-28 13:34:30 +0200

    Switch `sfFileLogger` from `strftime` to `date` for formatting time

--- test/unit/log/sfFileLoggerTest.php
+++ test/unit/log/sfFileLoggerTest.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the symfony package.
  * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -59,28 +59,28 @@ class TestLogger extends sfFileLogger
 // option: format
 $t->diag('option: format');
 unlink($file);
-$logger = new TestLogger($dispatcher, array('file' => $file));
+$logger = new TestLogger($dispatcher, ['file' => $file]);
 $logger->log('foo');
-$t->is(file_get_contents($file), strftime($logger->getTimeFormat()).' symfony [*6*] foo'.PHP_EOL, '->initialize() can take a format option');
+$t->is(file_get_contents($file), date($logger->getTimeFormat()).' symfony [*6*] foo'.PHP_EOL, '->initialize() can take a format option');
 
 unlink($file);
-$logger = new TestLogger($dispatcher, array('file' => $file, 'format' => '%message%'));
+$logger = new TestLogger($dispatcher, ['file' => $file, 'format' => '%message%']);
 $logger->log('foo');
 $t->is(file_get_contents($file), 'foo', '->initialize() can take a format option');
 
 // option: time_format
 $t->diag('option: time_format');
 unlink($file);
-$logger = new TestLogger($dispatcher, array('file' => $file, 'time_format' => '%Y %m %d'));
+$logger = new TestLogger($dispatcher, ['file' => $file, 'time_format' => '%Y %m %d']);
 $logger->log('foo');
-$t->is(file_get_contents($file), strftime($logger->getTimeFormat()).' symfony [*6*] foo'.PHP_EOL, '->initialize() can take a format option');
+$t->is(file_get_contents($file), date($logger->getTimeFormat()).' symfony [*6*] foo'.PHP_EOL, '->initialize() can take a format option');
 
 // option: type
 $t->diag('option: type');
 unlink($file);
-$logger = new TestLogger($dispatcher, array('file' => $file, 'type' => 'foo'));
+$logger = new TestLogger($dispatcher, ['file' => $file, 'type' => 'foo']);
 $logger->log('foo');
-$t->is(file_get_contents($file), strftime($logger->getTimeFormat()).' foo [*6*] foo'.PHP_EOL, '->initialize() can take a format option');
+$t->is(file_get_contents($file), date($logger->getTimeFormat()).' foo [*6*] foo'.PHP_EOL, '->initialize() can take a format option');
 
 // ->shutdown()
 $t->diag('->shutdown()');
